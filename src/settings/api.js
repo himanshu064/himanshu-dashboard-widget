0741ee5291be9266f7fed09d0a46b8f3752755dd
Himanshu Kumar <himanshu064@gmail.com>
2025-10-11 15:26:21 +0530

    exports issue fix

--- src/settings/api.js
+++ src/settings/api.js
@@ -1,71 +1,24 @@
 import { api } from "@telesero/frontend-common";
 import { getApiUrl, getBackofficeUrl } from "../constants";
 
-// Defer initialization until first use
-function initializeApi() {
-  const REACT_APP_API = getApiUrl();
-  if (!REACT_APP_API) {
-    console.warn(
-      "REACT_APP_API is not configured. Please set window.__WIDGET_CONFIG__.REACT_APP_API"
-    );
-    return api.createApi("");
-  }
-  return api.createApi(`${REACT_APP_API}/api`);
-}
+// Get API URLs with fallback to hardcoded values
+const REACT_APP_API = getApiUrl() || "https://portal.connx.cloud";
+const REACT_APP_BACKOFFICE = getBackofficeUrl() || "https://portal.connx.cloud";
 
-function initializeBackoffice() {
-  return api.createBackofficeApi();
-}
-
-// Create singleton instances with lazy initialization
-let _application;
-let _backoffice;
-
-const application = {};
-const backofficeApi = {};
-
-// Add getter/setter to lazily initialize
-Object.defineProperty(application, "_instance", {
-  get() {
-    if (!_application) {
-      _application = initializeApi();
-    }
-    return _application;
-  }
-});
-
-Object.defineProperty(backofficeApi, "_instance", {
-  get() {
-    if (!_backoffice) {
-      _backoffice = initializeBackoffice();
-    }
-    return _backoffice;
-  }
-});
-
-// Proxy to forward all method calls to the lazily-initialized instance
-const handler = {
-  get(target, prop) {
-    if (prop === "then" || prop === "catch" || prop === "finally") {
-      return undefined; // Not a promise
-    }
-    return target._instance[prop];
-  }
-};
-
-export default new Proxy(application, handler);
-export const backoffice = new Proxy(backofficeApi, handler);
+const application = api.createApi(`${REACT_APP_API}/api`);
+export default application;
+export const backoffice = api.createBackofficeApi();
 
 export const loginRedirect = () => {
-  const REACT_APP_BACKOFFICE = getBackofficeUrl();
-  return (window.location.href = `${REACT_APP_BACKOFFICE}/login?redirect_url=${encodeURI(
+  const backofficeUrl = getBackofficeUrl() || "https://portal.connx.cloud";
+  return (window.location.href = `${backofficeUrl}/login?redirect_url=${encodeURI(
     document.URL
   )}`);
 };
 
 export const logout = () => {
-  const REACT_APP_BACKOFFICE = getBackofficeUrl();
-  window.location.replace(`${REACT_APP_BACKOFFICE}/signout`);
+  const backofficeUrl = getBackofficeUrl() || "https://portal.connx.cloud";
+  window.location.replace(`${backofficeUrl}/signout`);
 };
 
 export const requestErrorHandler = api.requestErrorHandler;
