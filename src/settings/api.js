e7b91dd39a231e5c25f2eea4df98ec6e0f6266c3
Himanshu Kumar <himanshu064@gmail.com>
2025-10-11 14:34:48 +0530

    exports issue fix

--- src/settings/api.js
+++ src/settings/api.js
@@ -1,49 +1,60 @@
 import { api } from "@telesero/frontend-common";
 import { getApiUrl, getBackofficeUrl } from "../constants";
 
-// Lazy initialization to allow environment config to be set before API creation
-let application;
-let backofficeApi;
-
-const getApplication = () => {
-  if (!application) {
-    const REACT_APP_API = getApiUrl();
-    if (!REACT_APP_API) {
-      console.warn(
-        "REACT_APP_API is not configured. Please set window.__WIDGET_CONFIG__.REACT_APP_API"
-      );
-      return api.createApi(""); // Return a placeholder
-    }
-    application = api.createApi(`${REACT_APP_API}/api`);
+// Defer initialization until first use
+function initializeApi() {
+  const REACT_APP_API = getApiUrl();
+  if (!REACT_APP_API) {
+    console.warn(
+      "REACT_APP_API is not configured. Please set window.__WIDGET_CONFIG__.REACT_APP_API"
+    );
+    return api.createApi("");
   }
-  return application;
+  return api.createApi(`${REACT_APP_API}/api`);
+}
+
+function initializeBackoffice() {
+  return api.createBackofficeApi();
+}
+
+// Create singleton instances with lazy initialization
+let _application;
+let _backoffice;
+
+const application = {};
+const backofficeApi = {};
+
+// Add getter/setter to lazily initialize
+Object.defineProperty(application, '_instance', {
+  get() {
+    if (!_application) {
+      _application = initializeApi();
+    }
+    return _application;
+  }
+});
+
+Object.defineProperty(backofficeApi, '_instance', {
+  get() {
+    if (!_backoffice) {
+      _backoffice = initializeBackoffice();
+    }
+    return _backoffice;
+  }
+});
+
+// Proxy to forward all method calls to the lazily-initialized instance
+const handler = {
+  get(target, prop) {
+    if (prop === 'then' || prop === 'catch' || prop === 'finally') {
+      return undefined; // Not a promise
+    }
+    return target._instance[prop];
+  }
 };
 
-const getBackofficeApi = () => {
-  if (!backofficeApi) {
-    backofficeApi = api.createBackofficeApi();
-  }
-  return backofficeApi;
-};
-
-// Use a Proxy to lazily initialize the application when accessed
-export default new Proxy(
-  {},
-  {
-    get(target, prop) {
-      return getApplication()[prop];
-    }
-  }
-);
-
-export const backoffice = new Proxy(
-  {},
-  {
-    get(target, prop) {
-      return getBackofficeApi()[prop];
-    }
-  }
-);
+export default new Proxy(application, handler);
+export const backoffice = new Proxy(backofficeApi, handler);
 
 export const loginRedirect = () => {
   const REACT_APP_BACKOFFICE = getBackofficeUrl();
